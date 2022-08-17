e0cd0ebca55f54aad168b7393956edaaebfeabf9
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-08-17 14:21:04 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+//function declaration
+const divide = function (num1, num2){
+    num1=2000;
+    num2=100;
+    return num1/num2;
+
+}
+//single parameter arrow function
+const square = x => x ** 2;
+
+//arrow function with two parameters
+const add = (a,b) => a + b ;
